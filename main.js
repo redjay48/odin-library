250637929c3086ba4d1216441bf5856d087e7fc5
redjay48 <rajdey4892@gmail.com>
2022-09-16 13:22:01 +0530

    Add card structure

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        title: 'harry potter',
+        author: 'jk rowling',
+        noOfPages: 360,
+        readStatus: 'read'
+    }, {
+        title: 'harry potter 2',
+        author: 'j.k rowling',
+        noOfPages: 450,
+        readStatus: 'not read'
+    }, {
+        title: 'harry potter 3',
+        author: 'j.k. rowling',
+        noOfPages: 556,
+        readStatus: 'not read'
+    }];
 
 function Book(title, author, noOfPages, readStatus) {
     this.author = author
@@ -20,7 +36,14 @@ const title = document.getElementById('titleText');
 const author =  document.getElementById('authorText');
 const noOfPages = document.getElementById('noOfPagesText');
 const read = document.getElementById('readText');
-
+const card = document.querySelector('.card');
+const createButton = document.querySelector('#create');
+const deleteButton = document.querySelector('#delete');
+const cardDiv = document.querySelector('#book-cards');
+const cardTitle = document.querySelector('.card-title');
+const cardAuthor = document.querySelector('.card-author');
+const cardTotalPages = document.querySelector('.card-total-pages');
+const cardReadStatus = document.querySelector('.card-read-status');
 
 addButton.addEventListener('click', function() {
     if(formDiv.style.display === 'none') {
@@ -37,5 +60,22 @@ submitButton.addEventListener('click', function(e) {
         addBookToLibrary(book);
         document.querySelector('form').reset();
     }
-    
 })
+
+createButton.addEventListener('click', function() {
+    const newCard = document.createElement('div');
+    newCard.classList.add('card');
+    cardDiv.appendChild(newCard);
+    newCard.appendChild(cardTitle);
+    newCard.appendChild(cardAuthor);
+    newCard.appendChild(cardTotalPages);
+    newCard.appendChild(cardReadStatus);
+})
+
+deleteButton.addEventListener('click', function() {
+    cardDiv.lastChild.remove();
+})
+
+for (let i = 0; i < myLibrary.length; i++) {
+    
+}
